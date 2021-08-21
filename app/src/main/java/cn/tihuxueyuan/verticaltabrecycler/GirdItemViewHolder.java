efde0c9a4da6e9dd0d380eed2cd5367eba094aff
李刚 <ligang@ipfsforce.com>
2021-08-21 20:48:45 +0800

    移植左侧导航代码

---
+++ app/src/main/java/cn/tihuxueyuan/verticaltabrecycler/GirdItemViewHolder.java
@@ -0,0 +1,20 @@
+package cn.tihuxueyuan.verticaltabrecycler;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import cn.tihuxueyuan.R;
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * Create by hsw
+ * on 2018/12/3.
+ */
+public class GirdItemViewHolder extends RecyclerView.ViewHolder {
+    public TextView tvName;
+    public GirdItemViewHolder(Context context, ViewGroup parent) {
+        super(LayoutInflater.from(context).inflate(R.layout.vh_gird_item, parent, false));
+        tvName = itemView.findViewById(R.id.tv_name);
+    }
+}
