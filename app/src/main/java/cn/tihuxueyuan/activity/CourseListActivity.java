6c707792a9faf7d8057e8bcdae97159e0c9ce38d
李刚 <ligang@ipfsforce.com>
2021-08-23 07:58:46 +0800

    用\\.分割文件名， 取s[0] 使用标题和列表项名字

--- app/src/main/java/cn/tihuxueyuan/activity/CourseListActivity.java
+++ app/src/main/java/cn/tihuxueyuan/activity/CourseListActivity.java
@@ -1,5 +1,6 @@
 package cn.tihuxueyuan.activity;
 //
+
 import android.content.Intent;
 import android.os.Bundle;
 ////import android.support.v7.app.AppCompatActivity;
@@ -39,14 +40,15 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class CourseListActivity extends AppCompatActivity{
+public class CourseListActivity extends AppCompatActivity {
 
     final OkHttpClient client = new OkHttpClient();
     private android.widget.ListView lv;
     private CommonAdapter mAdapter;
-    private  String couseId;
-    private  String title;
+    private String couseId;
+    private String title;
     private List<CourseFileList.CourseFile> mList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,12 +61,13 @@ public class CourseListActivity extends AppCompatActivity{
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent=new Intent(getApplicationContext(),Music_Activity.class);//创建Intent对象，启动check
+                Intent intent = new Intent(getApplicationContext(), Music_Activity.class);//创建Intent对象，启动check
                 //将数据存入Intent对象
 //                player.setDataSource("http://47.102.146.8:8082/api/fileDownload?fileName=一声佛号一声心.mp3");
-                String musicUrl =  mList.get(position).getMp3url() + "?fileName=" + mList.get(position).getMp3FileName();
+                String musicUrl = mList.get(position).getMp3url() + "?fileName=" + mList.get(position).getMp3FileName();
                 intent.putExtra("music_url", musicUrl);
-                intent.putExtra("title", mList.get(position).getTitle());
+                String a[] = mList.get(position).getTitle().split("\\.");
+                intent.putExtra("title", a[0]);
 //                intent.putExtra("position",String.valueOf(position));
                 // ，
                 startActivity(intent);
@@ -72,7 +75,7 @@ public class CourseListActivity extends AppCompatActivity{
         });
 
 
-        Log.d("tag2", "onCreate: param: "+ couseId);
+        Log.d("tag2", "onCreate: param: " + couseId);
     }
 
     @Override
@@ -84,8 +87,24 @@ public class CourseListActivity extends AppCompatActivity{
     public void refreshListView() {
         lv.setAdapter(mAdapter = new CommonAdapter<CourseFile>(getApplicationContext(), mList, R.layout.dashboard_item_layout) {
             @Override
-            public void convertView(ViewHolder holder, CourseFile contactsBean) {
-                holder.set(R.id.name, contactsBean.getTitle());
+            public void convertView(ViewHolder holder, CourseFile courseFile) {
+//                String s = courseFile.getTitle();
+//                Log.d("tag1", "s: " + s);
+
+                /*
+                在正则表达式中是个已经被使用的特殊符号（"."、"|"、"^"等字符）
+所以想要使用 | ，必须用 \ 来进行转义，而在java字符串中，\ 也是个已经被使用的特殊符号，也需要使用 \ 来转义。
+所以应为：String[] all=str.split(("\\.")
+                 */
+                String a[] = courseFile.getTitle().split("\\.");
+//                if (a != null  && a[0] != null) {
+//                    holder.set(R.id.name, a[0]);
+//                }else{
+//                    holder.set(R.id.name,contactsBean.getTitle());
+//                }
+
+                holder.set(R.id.name, a[0]);
+
             }
         });
 
