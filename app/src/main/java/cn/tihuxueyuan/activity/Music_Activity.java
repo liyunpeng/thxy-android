3699a95141ff6f6f51891ba4e113518b9d3339ec
李刚 <ligang@ipfsforce.com>
2021-08-22 22:27:50 +0800

    音乐播放器进入时优化

--- app/src/main/java/cn/tihuxueyuan/activity/Music_Activity.java
+++ app/src/main/java/cn/tihuxueyuan/activity/Music_Activity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
@@ -20,74 +21,80 @@ import android.widget.TextView;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+
 import cn.tihuxueyuan.R;
 
 import cn.tihuxueyuan.fragment.list.ListFragment;
 import cn.tihuxueyuan.service.MusicService;
 
-public class Music_Activity extends AppCompatActivity implements View.OnClickListener{
+public class Music_Activity extends AppCompatActivity implements View.OnClickListener {
     private static SeekBar sb;
-    private static TextView tv_progress,tv_total,name_song;
+    private static TextView tv_progress, tv_total, name_song;
     private ObjectAnimator animator;
     public MusicService.MusicControl musicControl;
     String name;
-    Intent intent1,intent2;
+    Intent intent1, intent2;
     MyServiceConn conn;
     String musicUrl;
-    private boolean isUnbind =false;//记录服务是否被解绑
+    private boolean isUnbind = false;//记录服务是否被解绑
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
-        intent1=getIntent();
+        intent1 = getIntent();
         init();
     }
-    private void init(){
-        tv_progress=(TextView)findViewById(R.id.tv_progress);
-        tv_total=(TextView)findViewById(R.id.tv_total);
-        sb=(SeekBar)findViewById(R.id.sb);
-        name_song=(TextView)findViewById(R.id.song_name);
+
+    private void init() {
+        tv_progress = (TextView) findViewById(R.id.tv_progress);
+        tv_total = (TextView) findViewById(R.id.tv_total);
+        sb = (SeekBar) findViewById(R.id.sb);
+        name_song = (TextView) findViewById(R.id.song_name);
 
         findViewById(R.id.btn_play).setOnClickListener(this);
         findViewById(R.id.btn_pause).setOnClickListener(this);
         findViewById(R.id.btn_continue_play).setOnClickListener(this);
         findViewById(R.id.btn_exit).setOnClickListener(this);
 
-        name=intent1.getStringExtra("name");
+        name = intent1.getStringExtra("name");
         name_song.setText(name);
-        intent2=new Intent(this, MusicService.class);//创建意图对象
-        conn=new MyServiceConn();//创建服务连接对象
-        bindService(intent2,conn,BIND_AUTO_CREATE);//绑定服务
+        intent2 = new Intent(this, MusicService.class);//创建意图对象
+        conn = new MyServiceConn();//创建服务连接对象
+        bindService(intent2, conn, BIND_AUTO_CREATE);//绑定服务
         //为滑动条添加事件监听
         sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 //进度条改变时，会调用此方法
-                if (progress==seekBar.getMax()){//当滑动条到末端时，结束动画
+                if (progress == seekBar.getMax()) {//当滑动条到末端时，结束动画
                     animator.pause();//停止播放动画
                 }
             }
+
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {//滑动条开始滑动时调用
             }
+
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {//滑动条停止滑动时调用
                 //根据拖动的进度改变音乐播放进度
-                int progress=seekBar.getProgress();//获取seekBar的进度
+                int progress = seekBar.getProgress();//获取seekBar的进度
                 musicControl.seekTo(progress);//改变播放进度
             }
         });
-        ImageView iv_music=(ImageView)findViewById(R.id.iv_music);
+        ImageView iv_music = (ImageView) findViewById(R.id.iv_music);
 
 
 //        String position= intent1.getStringExtra("position");
-        String position= "1";
-        int i=parseInt(position);
+        String position = "1";
+        int i = parseInt(position);
         iv_music.setImageResource(ListFragment.icons[i]);
 
 
-        animator=ObjectAnimator.ofFloat(iv_music,"rotation",0f,360.0f);
+        animator = ObjectAnimator.ofFloat(iv_music, "rotation", 0f, 360.0f);
         animator.setDuration(10000);//动画旋转一周的时间为10秒
         animator.setInterpolator(new LinearInterpolator());//匀速
         animator.setRepeatCount(-1);//-1表示设置动画无限循环
@@ -96,72 +103,99 @@ public class Music_Activity extends AppCompatActivity implements View.OnClickLis
     }
 
 
-    public static Handler handler=new Handler(){//创建消息处理器对象
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    public static Handler handler = new Handler() {//创建消息处理器对象
         //在主线程中处理从子线程发送过来的消息
         @Override
-        public void handleMessage(Message msg){
-            Bundle bundle=msg.getData();//获取从子线程发送过来的音乐播放进度
-            int duration=bundle.getInt("duration");
-            int currentPosition=bundle.getInt("currentPosition");
+        public void handleMessage(Message msg) {
+            Log.d("tag1", "handleMessage: 处理消息 ： " + msg.toString());
+            Bundle bundle = msg.getData();//获取从子线程发送过来的音乐播放进度
+            int duration = bundle.getInt("duration");
+            int currentPosition = bundle.getInt("currentPosition");
             sb.setMax(duration);
             sb.setProgress(currentPosition);
             //歌曲总时长
-            int minute=duration/1000/60;
-            int second=duration/1000%60;
-            String strMinute=null;
-            String strSecond=null;
-            if(minute<10){//如果歌曲的时间中的分钟小于10
-                strMinute="0"+minute;//在分钟的前面加一个0
-            }else{
-                strMinute=minute+"";
+            int minute = duration / 1000 / 60;
+            int second = duration / 1000 % 60;
+            String strMinute = null;
+            String strSecond = null;
+            if (minute < 10) {//如果歌曲的时间中的分钟小于10
+                strMinute = "0" + minute;//在分钟的前面加一个0
+            } else {
+                strMinute = minute + "";
             }
-            if (second<10){//如果歌曲中的秒钟小于10
-                strSecond="0"+second;//在秒钟前面加一个0
-            }else{
-                strSecond=second+"";
+            if (second < 10) {//如果歌曲中的秒钟小于10
+                strSecond = "0" + second;//在秒钟前面加一个0
+            } else {
+                strSecond = second + "";
             }
-            tv_total.setText(strMinute+":"+strSecond);
+            tv_total.setText(strMinute + ":" + strSecond);
             //歌曲当前播放时长
-            minute=currentPosition/1000/60;
-            second=currentPosition/1000%60;
-            if(minute<10){//如果歌曲的时间中的分钟小于10
-                strMinute="0"+minute;//在分钟的前面加一个0
-            }else{
-                strMinute=minute+" ";
+            minute = currentPosition / 1000 / 60;
+            second = currentPosition / 1000 % 60;
+            if (minute < 10) {//如果歌曲的时间中的分钟小于10
+                strMinute = "0" + minute;//在分钟的前面加一个0
+            } else {
+                strMinute = minute + " ";
             }
-            if (second<10){//如果歌曲中的秒钟小于10
-                strSecond="0"+second;//在秒钟前面加一个0
-            }else{
-                strSecond=second+" ";
+            if (second < 10) {//如果歌曲中的秒钟小于10
+                strSecond = "0" + second;//在秒钟前面加一个0
+            } else {
+                strSecond = second + " ";
             }
-            tv_progress.setText(strMinute+":"+strSecond);
+            tv_progress.setText(strMinute + ":" + strSecond);
         }
     };
-    class MyServiceConn implements ServiceConnection{//用于实现连接服务
+
+    class MyServiceConn implements ServiceConnection {//用于实现连接服务
+
         @Override
-        public void onServiceConnected(ComponentName name, IBinder service){
-            musicControl=(MusicService.MusicControl) service;
+        public void onServiceConnected(ComponentName name, IBinder service) {
+
+            Log.d("tag1", "onServiceConnected: 服务连接成功 ");
+            musicControl = (MusicService.MusicControl) service;
+            musicControl.init(musicUrl);
+
+            new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        Thread.sleep(1000);
+                        musicControl.setText();
+                        musicControl.play();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+//                            handler.sendMessage();
+                }
+            }).start();
         }
+
         @Override
-        public void onServiceDisconnected(ComponentName name){
+        public void onServiceDisconnected(ComponentName name) {
 
         }
     }
-    private void unbind(boolean isUnbind){
-        if(!isUnbind){//判断服务是否被解绑
+
+    private void unbind(boolean isUnbind) {
+        if (!isUnbind) {//判断服务是否被解绑
             musicControl.pausePlay();//暂停播放音乐
             unbindService(conn);//解绑服务
         }
     }
+
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn_play://播放按钮点击事件
 //                String position=intent1.getStringExtra("position");
 //                int i=parseInt(position);
 //                musicControl.play(i);
-                musicControl.play(musicUrl);
+                musicControl.play();
                 animator.start();
                 break;
             case R.id.btn_pause://暂停按钮点击事件
@@ -174,13 +208,14 @@ public class Music_Activity extends AppCompatActivity implements View.OnClickLis
                 break;
             case R.id.btn_exit://退出按钮点击事件
                 unbind(isUnbind);
-                isUnbind=true;
+                isUnbind = true;
                 finish();
                 break;
         }
     }
+
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
         unbind(isUnbind);//解绑服务
     }
--- app/src/main/java/cn/tihuxueyuan/service/MusicService.java
+++ app/src/main/java/cn/tihuxueyuan/service/MusicService.java
@@ -7,6 +7,7 @@ import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Message;
+
 import cn.tihuxueyuan.activity.Music_Activity;
 
 import java.io.IOException;
@@ -16,44 +17,81 @@ import java.util.TimerTask;
 public class MusicService extends Service {
     public MediaPlayer player;
     public Timer timer;
-    public MusicService() {}
+
+    public MusicService() {
+    }
+
     @Override
-    public  IBinder onBind(Intent intent){
+    public IBinder onBind(Intent intent) {
         return new MusicControl();
     }
+
     @Override
-    public void onCreate(){
+    public void onCreate() {
         super.onCreate();
-        player=new MediaPlayer();//创建音乐播放器对象
+        player = new MediaPlayer();//创建音乐播放器对象
 
     }
-    public void addTimer(){ //添加计时器用于设置音乐播放器中的播放进度条
-        if(timer==null){
-            timer=new Timer();//创建计时器对象
-            TimerTask task=new TimerTask() {
+
+    public void addTimer() { //添加计时器用于设置音乐播放器中的播放进度条
+        if (timer == null) {
+            timer = new Timer();//创建计时器对象
+            TimerTask task = new TimerTask() {
                 @Override
                 public void run() {
-                    if (player==null) return;
-                    int duration=player.getDuration();//获取歌曲总时长
-                    int currentPosition=player.getCurrentPosition();//获取播放进度
-                    Message msg=Music_Activity.handler.obtainMessage();//创建消息对象
+                    if (player == null) return;
+                    int duration = player.getDuration();//获取歌曲总时长
+                    int currentPosition = player.getCurrentPosition();//获取播放进度
+                    Message msg = Music_Activity.handler.obtainMessage();//创建消息对象
                     //将音乐的总时长和播放进度封装至消息对象中
-                    Bundle bundle=new Bundle();
-                    bundle.putInt("duration",duration);
-                    bundle.putInt("currentPosition",currentPosition);
+                    Bundle bundle = new Bundle();
+                    bundle.putInt("duration", duration);
+                    bundle.putInt("currentPosition", currentPosition);
                     msg.setData(bundle);
                     //将消息发送到主线程的消息队列
                     Music_Activity.handler.sendMessage(msg);
                 }
             };
             //开始计时任务后的5毫秒，第一次执行task任务，以后每500毫秒执行一次
-            timer.schedule(task,5,500);
+            timer.schedule(task, 5, 500);
         }
     }
-    public class MusicControl extends Binder{ //Binder是一种跨进程的通信方式
-        public void play(String url){//String path
+
+    public class MusicControl extends Binder { //Binder是一种跨进程的通信方式
+
+        public void setText() {
+            if (player == null) return;
+            int duration = player.getDuration();//获取歌曲总时长
+            int currentPosition = player.getCurrentPosition();//获取播放进度
+            Message msg = Music_Activity.handler.obtainMessage();//创建消息对象
+            //将音乐的总时长和播放进度封装至消息对象中
+            Bundle bundle = new Bundle();
+            bundle.putInt("duration", duration);
+            bundle.putInt("currentPosition", currentPosition);
+            msg.setData(bundle);
+            //将消息发送到主线程的消息队列
+            Music_Activity.handler.sendMessage(msg);
+        }
+        public void init(String url) {
+            player.reset();
+            try {
+//                        player.setDataSource("http://47.102.146.8:8082/api/fileDownload?fileName=一声佛号一声心.mp3");
+                player.setDataSource(url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            player.prepareAsync();
+
+
+            setText();
+
+
+        }
+
+        public void play() {//String path
 //                Uri uri=Uri.parse("android.resource://"+getPackageName()+"/raw/"+"music"+i);
-                try{
+            try {
+                player.start(); //播放音乐
 /*
                     player.reset(); //重置音乐播放器
                     //加载多媒体文件
@@ -61,53 +99,35 @@ public class MusicService extends Service {
                     player.start(); //播放音乐
 */
 
-                    player.reset();
-                    try {
-//                        player.setDataSource("http://47.102.146.8:8082/api/fileDownload?fileName=一声佛号一声心.mp3");
-                        player.setDataSource(url);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    player.prepareAsync();
-
-
-                    new Thread(new Runnable() {
-                        public void run() {
-                            //sleep设置的是时长
-                            try {
-                                Thread.sleep(1000);
-                                player.start(); //播放音乐
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-//                            handler.sendMessage();
-                        }
-                    }).start();
 
 
 
 
-                    addTimer();//添加计时器
-                }catch(Exception e){
+                addTimer();//添加计时器
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        public void pausePlay(){
+
+        public void pausePlay() {
             player.pause();//暂停播放音乐
         }
-        public void continuePlay(){
+
+        public void continuePlay() {
             player.start();//继续播放音乐
         }
-        public void seekTo(int progress){
+
+        public void seekTo(int progress) {
             player.seekTo(progress);//设置音乐的播放位置
         }
     }
+
     @Override
-    public void onDestroy(){
+    public void onDestroy() {
         super.onDestroy();
-        if(player==null) return;
-        if(player.isPlaying()) player.stop();//停止播放音乐
+        if (player == null) return;
+        if (player.isPlaying()) player.stop();//停止播放音乐
         player.release();//释放占用的资源
-        player=null;//将player置为空
+        player = null;//将player置为空
     }
 }
