d9018bc7c862f3c264c927b00e8846a70d84fd44
李刚 <ligang@ipfsforce.com>
2021-10-25 20:10:52 +0800

    注释掉最新

--- app/src/main/java/cn/tihuxueyuan/activity/MainActivity.java
+++ app/src/main/java/cn/tihuxueyuan/activity/MainActivity.java
@@ -213,7 +213,8 @@ public class MainActivity extends BaseActivity {
         mContext = this.getApplicationContext();
         BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration
-                .Builder(R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+//                .Builder(R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .Builder( R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
